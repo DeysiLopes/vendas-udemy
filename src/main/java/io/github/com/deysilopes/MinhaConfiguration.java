a7e9f2e465d8b3501ec637ccdb0b6960a1a91d4a
DeysiLopes <deysilinconl@gmail.com>
2024-04-12 21:58:17 -0300

    feat(config profile): config profile

--- src/main/java/io/github/com/deysilopes/MinhaConfiguration.java
+++ src/main/java/io/github/com/deysilopes/MinhaConfiguration.java
@@ -1,9 +1,19 @@
 package io.github.com.deysilopes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.CommandLinePropertySource;
 
 @Configuration
+@Profile("dev")
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando config de dev");
+        };
+    }
 }
